454ee59eeda21f2a5ee95acddf7340f33e2d0ad1
Dan Rusu <dangingko@yahoo.com>
2025-04-30 19:11:22 +0300

    adding assertions

--- src/test/java/com/testutils/web/DevToolsPoc.java
+++ src/test/java/com/testutils/web/DevToolsPoc.java
@@ -8,18 +8,25 @@ import org.openqa.selenium.devtools.v131.network.Network;
 import org.openqa.selenium.devtools.v131.network.model.Request;
 import org.openqa.selenium.devtools.v131.network.model.Response;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static utils.logger.SimpleLogger.log;
 import static utils.webdriver.DevToolsUtil.getDevToolsSession;
 import static utils.webdriver.DriverUtil.waitForPageToLoad;
 
 public class DevToolsPoc extends UIBaseTest {
     @Test
-    void testDevToolsRequestsListener() {
+    void testRequests() {
         DevTools devTools = getDevToolsSession(driver);
+        List<String> urls = new ArrayList<>();
         try {
 
             devTools.addListener(Network.requestWillBeSent(), requestEvent -> {
                 Request request = requestEvent.getRequest();
+                urls.add(request.getUrl());
                 log(String.format("%s %s", request.getMethod(), request.getUrl()));
             });
 
@@ -31,15 +38,21 @@ public class DevToolsPoc extends UIBaseTest {
             devTools.send(Network.disable());
             devTools.close();
         }
+
+        assertFalse(urls.isEmpty());
     }
 
+
     @Test
-    void testDevToolsResponseListener() {
+    void testResponses() {
         DevTools devTools = getDevToolsSession(driver);
+
+        List<Integer> statusCodes = new ArrayList<>();
         try {
 
             devTools.addListener(Network.responseReceived(), responseReceived -> {
                 Response response = responseReceived.getResponse();
+                statusCodes.add(response.getStatus());
                 log(String.format("%s %s", response.getStatus(), response.getUrl()));
             });
 
@@ -51,5 +64,9 @@ public class DevToolsPoc extends UIBaseTest {
             devTools.send(Network.disable());
             devTools.close();
         }
+
+        statusCodes.forEach(
+                statusCode -> assertEquals(200, statusCode)
+        );
     }
 }
