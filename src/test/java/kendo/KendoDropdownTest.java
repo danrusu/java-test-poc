75899b72fbff2fdec70813bbc3d13964ef4b7be4
Dan Rusu <dangingko@yahoo.com>
2025-06-12 13:15:34 +0300

    kendo ui component utils

--- src/test/java/kendo/KendoDropdownTest.java
+++ src/test/java/kendo/KendoDropdownTest.java
@@ -1,26 +1,37 @@
 package kendo;
 
 import com.base.UIBaseTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import utils.KendoWebComponent;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static utils.KendoUtil.getSelectedDropdownListOption;
-import static utils.KendoUtil.selectDropdownListOption;
 
 
 public class KendoDropdownTest extends UIBaseTest {
-    public static String KENDO_DROPDOWN_LIST_URL =
+    public static final String KENDO_DROPDOWN_LIST_URL =
             "https://demos.telerik.com/kendo-angular-ui/demos/dropdowns/dropdownlist/basic_usage" +
                     "?theme=default-ocean-blue-a11y";
+    public KendoWebComponent kendoWebComponent;
+
+    @BeforeEach
+    void beforeEach() {
+        kendoWebComponent = new KendoWebComponent(driver);
+    }
 
     @Test
     void testOptionSelect() {
         final String optionToSelect = "Boston";
         driver.get(KENDO_DROPDOWN_LIST_URL);
-        selectDropdownListOption(driver, optionToSelect);
+        kendoWebComponent.selectDropdownListOption(optionToSelect);
         assertEquals(
                 optionToSelect,
-                getSelectedDropdownListOption(driver)
+                kendoWebComponent.getSelectedDropdownListOption()
         );
+    }
+
+    //    https://www.telerik.com/kendo-angular-ui/components/inputs/formfield
+    @Test
+    void testFormFieldRadio() {
     }
 }
--- src/test/java/utils/KendoWebComponent.java
+++ src/test/java/utils/KendoWebComponent.java
@@ -3,26 +3,27 @@ package utils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import utils.base.StaticClass;
 
 import java.time.Duration;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
-public class KendoUtil extends StaticClass {
+public class KendoWebComponent {
+    public static final By DROPDOWN_LIST_SELECTOR = By.cssSelector("kendo-dropdownlist");
+    public static final String DROPDOWN_LIST_POPUP_TAG_SELECTOR = "kendo-popup";
 
-    public static final By DROPDOWN_LIST_DEFAULT_SELECTOR = By.cssSelector("kendo-dropdownlist");
+    private final WebDriver driver;
+    private final WebDriverWait wait;
 
-    public static void selectDropdownListOption(
-            WebDriver driver,
+    public KendoWebComponent(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+    public void selectDropdownListOption(
             By dropdownListSelector,
             String optionText) {
-
-        var wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        // open dropdown
         wait.until(visibilityOfElementLocated(dropdownListSelector)).click();
-
-        // click option
         String optionXpath = String.format(
                 "//kendo-popup//*[normalize-space(text())='%s']",
                 optionText
@@ -31,20 +32,17 @@ public class KendoUtil extends StaticClass {
         wait.until(visibilityOfElementLocated(optionSelector)).click();
     }
 
-    public static void selectDropdownListOption(WebDriver driver, String optionText) {
-        selectDropdownListOption(
-                driver,
-                DROPDOWN_LIST_DEFAULT_SELECTOR,
-                optionText);
+    public void selectDropdownListOption(String optionText) {
+        selectDropdownListOption(DROPDOWN_LIST_SELECTOR, optionText);
     }
 
-    public static String getSelectedDropdownListOption(WebDriver driver, By dropdownListSelector) {
+    public String getSelectedDropdownListOption(By dropdownListSelector) {
         return driver.findElement(dropdownListSelector)
                 .findElement(By.cssSelector(".k-input-value-text"))
                 .getText();
     }
 
-    public static String getSelectedDropdownListOption(WebDriver driver) {
-        return getSelectedDropdownListOption(driver, DROPDOWN_LIST_DEFAULT_SELECTOR);
+    public String getSelectedDropdownListOption() {
+        return getSelectedDropdownListOption(DROPDOWN_LIST_SELECTOR);
     }
 }
